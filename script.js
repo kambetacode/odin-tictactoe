2e695681d178d55bc3656e07adc2821ec06c3cb1
Kevin Mora <kambetacode@gmail.com>
2023-10-27 17:38:19 -0500

    Added new winCombination array to store possible winning combinations

--- script.js
+++ script.js
@@ -18,6 +18,16 @@ Tic tac toe game, what does it have? =>
 // Interface
 const main = document.querySelector('main')
 const gameBoardDiv = document.getElementById('game-board')
+const winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+] 
 
 //
 
