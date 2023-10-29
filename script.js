a440085559c14453d60d79d5f88bc5d3b6f238c8
Kevin Mora <kambetacode@gmail.com>
2023-10-29 17:56:21 -0500

    Added new functionality to check games winner
    Added new player1 and player2 objects to handle each players clicks combination
    Added new handlePlayer function to achieve the above using the players objects
    Added new handleWin function to check if the players combination matches any possible winning combination on winCombination array

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/*
-Brainstorm:
-
-Tic tac toe game, what does it have? =>
-
--gameboard
--players
-
--how the gameboard will be? 
-    each gameboard box will be in an array (stored in an object)
-    goal:   little global code as possible  
-    If I need ONE of something: use a module
-    If I need multiple of something: use factories
-
-
-*/
-
 // Interface
 const main = document.querySelector('main')
 const gameBoardDiv = document.getElementById('game-board')
@@ -36,6 +19,17 @@ const gameBoard = (function() {
     let gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     let gameSymbols = [null, null, null, null, null, null, null, null, null]
     let player = true
+    
+    let player1 = {
+        symbol: 'X',
+        combination: []
+    }
+
+    let player2 = {
+        symbol: 'O',
+        combination: []
+    }
+
 
 
     const drawGameBoard = () => {
@@ -47,6 +41,7 @@ const gameBoard = (function() {
 
             box.addEventListener('click', (e) => {
                 drawSymbol(e, e.target.dataset.index)
+                handleWin()
             })
 
             gameBoardDiv.appendChild(box)
@@ -58,10 +53,12 @@ const gameBoard = (function() {
             if(player) {
                 e.target.innerHTML = 'X'
                 handleGameSymbols(index)
+                handlePlayer(true, index)
                 
             } else {
                 e.target.innerHTML = 'O'
                 handleGameSymbols(index)
+                handlePlayer(false, index)
             }
         } else {
         }
@@ -72,9 +69,27 @@ const gameBoard = (function() {
         player = !player
     }
 
+    const handlePlayer = (handler, target) => {
+        if(handler) {
+            player1.combination.push(parseInt(target))
+        } else {
+            player2.combination.push(parseInt(target))
+        }
+    }
+
+    const handleWin = () => {
+        winCombinations.forEach(item => {
+            if(item.every(item => player1.combination.includes(item))) {
+                alert('X wins, end of the game')
+            } else if (item.every(item => player2.combination.includes(item))) {
+                console.log('O wins, end of the game')
+            }
+        })
+    }
+
 
     return {
-        drawGameBoard,
+        drawGameBoard
     }
 })();
 
